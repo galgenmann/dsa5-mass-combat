b7a72de2bed89510063f1bc80a823165ffaafca6
Paul Jena <jpaul.devel@gmail.com>
2023-11-12 03:06:23 +0100

    Add automatic rendering of ship sheet on startup

--- modules/init.js
+++ modules/init.js
@@ -1,3 +1,4 @@
+import DSA5Dialog from "../../../systems/dsa5/modules/dialog/dialog-dsa5.js";
 import ActorSheetdsa5Ship from "./ship_sheet.js";
 
 Hooks.on("init", async function() {
@@ -5,6 +6,10 @@ Hooks.on("init", async function() {
   console.log("Initializing DSA4 Naval Combat module");
 });
 
-Hooks.once("ready", function() {
-    console.log(game.dsa5);
+Hooks.once("ready", async function() {
+    // for development purposes: will render the first actor sheet, which has an ship sheet configured as its active sheet
+    // results in rendering the sheet when refreshing with F5 automatacially
+    game.actors.contents.find(element => {
+        return element.sheet instanceof ActorSheetdsa5Ship;
+    }).sheet.render(true);
 });
