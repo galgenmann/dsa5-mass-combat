054b386c6a4246c83c45be8b45327f88819495e8
teliogeo <g.telios@outlook.de>
2023-11-18 17:27:54 +0100

    improvements in ship sheet

--- modules/ship_sheet.js
+++ modules/ship_sheet.js
@@ -1,10 +1,23 @@
 import ActorSheetDsa5 from "../../../systems/dsa5/modules/actor/actor-sheet.js";
 
+function prepareShipFlags(actor) {
+    console.log(actor.id);
+    actor.setFlag("dsa5_ship_combat","crew", {
+        "rowers":0,
+        "soliders": 0,
+        "officers": 0,
+        "sailors": 0,
+        "gunners": 0
+    });
+}
+
 export default class ActorSheetdsa5Ship extends ActorSheetDsa5 {
     static async registerSheet(){
         Actors.registerSheet("dsa5", ActorSheetdsa5Ship, { types: ["character"] });
         game.dsa5.sheets.ActorSheetdsa5Ship = ActorSheetdsa5Ship;
         await loadTemplates(['modules/dsa5_ship_combat/templates/ship_main.html']);
+        await loadTemplates(['modules/dsa5_ship_combat/templates/ship_combat.html']);
+        await loadTemplates(['modules/dsa5_ship_combat/templates/ship_inventory.html']);
         console.log(game.dsa5.sheets.ActorSheetdsa5Ship);
     }
 
@@ -21,8 +34,18 @@ export default class ActorSheetdsa5Ship extends ActorSheetDsa5 {
     async _render(force = false, options = {}) {
         await super._render(force, options);
     }
+    
+    async getData(options) {
+        if (this.object.flags.dsa5_ship_combat === undefined && this.object.isOwner) {
+            prepareShipFlags(this.object);
+        }
+        const data = await super.getData(options);
+        data.ship = data.actor.flags.dsa5_ship_combat;
 
+        return data;
 
+    }
+    
     get template() {
         return "./modules/dsa5_ship_combat/templates/ship_sheet.html";
     }
